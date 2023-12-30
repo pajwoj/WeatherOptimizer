8aae40356b0bf76947cac00faf98e88e6ecacd6f
Wojciech Pajtel <pajwoj@gmail.com>
2023-12-30 19:56:48 +0100

    add frontend - release v1.0

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # WeatherOptimizer
 Weather forecast optimizer, engineering thesis project
+Written in Java 8, uses Spring, JQuery
+
+# How to use:
+
+Download Java 8 (https://www.java.com/en/download/manual.jsp),
+Run the latest release.
+
+To get a weather forecast use the tray icon or open localhost:8080 in the browser and use the text box to input a location.
 
 todo:
-
-- generate table with data clientside https://api.jquery.com/append/ (first day one hour less, put generating table headers in a different function as they are always the same, start with precipitation chance, sunrise, sunset as they are easier)
 - thesis ;)
-
-raw result text to put in json viewer: https://pastebin.com/NmLhJiSa
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,26 +1,29 @@
 let jsonResponse;
 let dayOfWeek = 0
+let keys = "";
+let _7TimerDaySkip = 0;
+let _7TimerDaySkip2 = 0;
 
 $.ajax({
-    url: "http://localhost:8080/api/results" + document.location.search,
-    success: function (data, textStatus, xhr) {
+    url: "http://localhost:8080/api/results" + document.location.search, success: function (data) {
         jsonResponse = data;
-        let keys = Object.keys(jsonResponse)
+        keys = Object.keys(jsonResponse);
 
         keys.push(keys.splice(keys.indexOf("Average"), 1)[0]);
 
         $("#content").load("results-content.html", function () {
             $("#res-header").text("Results for: " + jsonResponse.OpenMeteo[0].location.locationString);
-            $("#res-day").text("Current date: " + getDateString(jsonResponse.OpenMeteo[0].date, "pl-PL"));
+            document.title = "Weather Optimizer - " + jsonResponse.OpenMeteo[0].location.locationString;
 
-            // $("#previous-day-button").on("click", function () { previousData() });
-            // $("#next-day-button").on("click", function () { nextData() });
+            _7TimerDaySkip = _7TimerDaySkip2 = 7 - jsonResponse["7Timer"][0].times.length;
 
+            loadData();
         });
     },
 
-    error: function (xhr, textStatus) {
-        document.getElementById("content").innerHTML = "Error when getting location data (wrong location?). Try again!"
+    error: function () {
+        document.getElementById("content").innerHTML = "Error when getting location data (wrong location?). Try again!" + "<br><br><a href=\"index.html\">Back to main page</a>"
+        document.title = "Error! - Weather Optimizer";
     }
 });
 
@@ -30,7 +33,18 @@ function getDateString(dateString, locale) {
 }
 
 function loadData() {
-    $("#test-content").html("<h1>" + dayOfWeek + "</h1>")
+    $("#res-day").text("Current date: " + getDateString(jsonResponse.OpenMeteo[dayOfWeek].date, "pl-PL"));
+
+    clearTables();
+    prepareTables();
+}
+
+function clearTables() {
+    $("#temperatures-table").html("");
+    $("#precipitation-table").html("");
+    $("#precipitation-chance-table").html("");
+    $("#sunrise-table").html("");
+    $("#sunset-table").html("");
 }
 
 function previousData() {
@@ -40,8 +54,7 @@ function previousData() {
     } else if (dayOfWeek === 6) {
         dayOfWeek--;
         $("#next-day-button").prop("disabled", false);
-    } else
-        dayOfWeek--;
+    } else dayOfWeek--;
 
     loadData();
 
@@ -54,16 +67,185 @@ function nextData() {
     } else if (dayOfWeek === 5) {
         dayOfWeek++;
         $("#next-day-button").prop("disabled", true);
-    } else
-        dayOfWeek++;
+    } else dayOfWeek++;
 
     loadData();
 }
 
-function th(text) {
-    return "<th>" + text + "</th>";
+function addTableHeaders(keysArray) {
+    let result = "";
+
+    keysArray.forEach((key) => {
+        result += ("<th>" + key + "</th>");
+    });
+
+    return result;
 }
 
-function td(text) {
+function prepareTables() {
+    $("#temperatures-table").append(addTr("<th>Hour</th>" + addTableHeaders(keys) + addTemperature()));
+    $("#precipitation-table").append(addTr("<th>Hour</th>" + addTableHeaders(keys) + addPrecipitation(keys)));
+
+    $("#precipitation-chance-table").append(addTr(addTableHeaders(keys) + addPrecipitationChance()));
+    $("#sunrise-table").append(addTr(addTableHeaders(keys) + addSunrise()));
+    $("#sunset-table").append(addTr(addTableHeaders(keys) + addSunset()));
+}
+
+function addTd(text) {
     return "<td>" + text + "</td>";
+}
+
+function addTr(text) {
+    return "<tr>" + text + "</tr>";
+}
+
+function addTemperature() {
+    let result = "";
+
+    let timeAmount = jsonResponse.Average[dayOfWeek].times.length;
+
+    for (let i = 0; i < timeAmount; i++) {
+        result += addTd(jsonResponse.Average[dayOfWeek].times[i]);
+
+        for (const [key, value] of Object.entries(jsonResponse)) {
+            if (value == null) {
+                result += addTd("-");
+                continue;
+            }
+
+            if (key === "Average") continue;
+
+            else {
+                if (dayOfWeek === 0 && key === "7Timer" && _7TimerDaySkip > 0) {
+                    result += addTd("-");
+                    _7TimerDaySkip--;
+                }
+
+                else {
+                    value[dayOfWeek].times.forEach((element, index) => {
+                        if (element === jsonResponse.Average[dayOfWeek].times[i]) {
+                            result += addTd(value[dayOfWeek].temps[index] + "°C");
+                        }
+                    });
+                }
+            }
+        }
+
+        result += addTd(jsonResponse.Average[dayOfWeek].temps[i] + "°C");
+        result = addTr(result);
+    }
+
+    return result;
+}
+
+function addPrecipitation() {
+    let result = "";
+
+    let timeAmount = jsonResponse.Average[dayOfWeek].times.length;
+
+    for (let i = 0; i < timeAmount; i++) {
+        result += addTd(jsonResponse.Average[dayOfWeek].times[i]);
+
+        for (const [key, value] of Object.entries(jsonResponse)) {
+            if (value == null) {
+                result += addTd("-");
+                continue;
+            }
+
+            if (key === "Average") continue;
+
+            else {
+                if (dayOfWeek === 0 && key === "7Timer" && _7TimerDaySkip2 > 0) {
+                    result += addTd("-");
+                    _7TimerDaySkip2--;
+                }
+
+                else {
+                    value[dayOfWeek].times.forEach((element, index) => {
+                        if (element === jsonResponse.Average[dayOfWeek].times[i]) {
+                            if(isNaN(Number(value[dayOfWeek].precipitation[index])))
+                                result += addTd("-");
+
+                            else
+                                result += addTd(value[dayOfWeek].precipitation[index] + "mm");
+                        }
+                    });
+                }
+            }
+        }
+
+        result += addTd(jsonResponse.Average[dayOfWeek].precipitation[i] + "mm");
+        result = addTr(result);
+    }
+
+    return result;
+}
+
+function addPrecipitationChance() {
+    let result = "";
+
+    for (const [key, value] of Object.entries(jsonResponse)) {
+        if (value == null) {
+            result += addTd("-");
+            continue;
+        }
+
+        if (key === "Average") continue;
+
+        else {
+            if (value[dayOfWeek].precipitationChance <= 100 && value[dayOfWeek].precipitationChance >= 0) result += addTd(value[dayOfWeek].precipitationChance + "%");
+
+            else result += addTd("-");
+        }
+    }
+
+    result += addTd(jsonResponse.Average[dayOfWeek].precipitationChance + "%")
+
+    return addTr(result);
+}
+
+function addSunrise() {
+    let result = "";
+
+    for (const [key, value] of Object.entries(jsonResponse)) {
+        if (value == null) {
+            result += addTd("-");
+            continue;
+        }
+
+        if (key === "Average") continue;
+
+        else {
+            if (value[dayOfWeek].sunrise != null) result += addTd(value[dayOfWeek].sunrise);
+
+            else result += addTd("-");
+        }
+    }
+
+    result += addTd(jsonResponse.Average[dayOfWeek].sunrise)
+
+    return addTr(result);
+}
+
+function addSunset() {
+    let result = "";
+
+    for (const [key, value] of Object.entries(jsonResponse)) {
+        if (value == null) {
+            result += addTd("-");
+            continue;
+        }
+
+        if (key === "Average") continue;
+
+        else {
+            if (value[dayOfWeek].sunset != null) result += addTd(value[dayOfWeek].sunset);
+
+            else result += addTd("-");
+        }
+    }
+
+    result += addTd(jsonResponse.Average[dayOfWeek].sunset)
+
+    return addTr(result);
 }
